0759fe96330c02c4a13c9f1f04f0aa8202a78a44
linwexEU <130746152+linwexEU@users.noreply.github.com>
2025-03-18 14:46:41 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,22 +1,11 @@
 from fastapi import FastAPI
 
-from contextlib import asynccontextmanager
-
 from src.api import api_router
 from src.logging import configure_logging
-from src.redis.redis_client import RedisClient
-
-
-@asynccontextmanager 
-async def lifespan(app: FastAPI):
-    redis_client = RedisClient() 
-    redis_client.connect() 
-    yield 
-    redis_client.disconnet() 
 
 
 def create_app() -> FastAPI: 
-    app = FastAPI(lifespan=lifespan) 
+    app = FastAPI() 
     return app 
 
 
